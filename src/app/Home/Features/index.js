ab9647fe2ea2bcb80f1ac48382aa016f95a2265c
smiledental <raghubabukamarajugadda@gmail.com>
2025-02-22 02:45:49 +0530

    modified:   src/app/Home/Features/index.js

--- src/app/Home/Features/index.js
+++ src/app/Home/Features/index.js
@@ -12,7 +12,7 @@ export default function Features(props) {
             <div className="col-lg-12">
               <SectionHead
                 title="We Are Always Ready to Help You & Your Family"
-                desc="Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts"
+                desc="Where compassion meets cutting-edge dental care. Your family's oral health is our highest priority, supported by 25+ years of trusted expertise."
               />
             </div>
           </div>
@@ -21,26 +21,24 @@ export default function Features(props) {
               <FeatureCard
                 cardClass="single-features"
                 icon="icofont icofont-ambulance-cross"
-                title="Emergency Help"
-                desc="Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam vulputate."
+                title="24/7 Dental Emergency"
+                desc="Immediate care for toothaches, injuries, and urgent cases. Our team is always on standby with pain-relief solutions."
               />
             </div>
             <div className="col-lg-4 col-12">
               <FeatureCard
                 cardClass="single-features"
                 icon="icofont icofont-medical-sign-alt"
-                title="Enriched Pharmecy"
-                desc="Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam
-                vulputate."
+                title="Dental Pharmacy"
+                desc="Complete range of oral care products, branded medications, and post-treatment kits available at all branches."
               />
             </div>
             <div className="col-lg-4 col-12">
               <FeatureCard
                 cardClass="single-features last"
                 icon="icofont icofont-stethoscope"
-                title="Medical Treatment"
-                desc="Lorem ipsum sit, consectetur adipiscing elit. Maecenas mi quam
-                vulputate."
+                title="Specialist Consultations"
+                desc="Personalized treatment plans from implant surgeons, orthodontists, and pediatric dental experts."
               />
             </div>
           </div>
