fa00a2f20990b4fec5def16a7921960015ae4341
Raghu <raghu@example.com>
2025-04-06 23:58:31 +0530

    modified:   src/components/ServiceCard/index.js

--- src/components/ServiceCard/index.js
+++ src/components/ServiceCard/index.js
@@ -15,7 +15,7 @@ export default function ServiceCard(props) {
         <p>
           {desc
             ? desc
-            : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut is imperdiet."}
+            : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet."}
         </p>
       </div>
     </>
